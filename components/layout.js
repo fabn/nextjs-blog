2e913e7863591d17ce09ea7ce9ef0e4876aa5882
Fabio Napoleoni <f.napoleoni@gmail.com>
2020-07-30 13:29:49 +0200

    Add custom page with fetch data

--- components/layout.js
+++ components/layout.js
@@ -3,8 +3,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'fabn on Next.js'
+export const siteTitle = 'Next.js Sample Website with dynamic data'
 
 export default function Layout({ children, home }) {
   return (
---
+++ pages/whatismyip.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import Layout, {siteTitle} from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import {useEffect, useState} from "react";
+
+function MyIpAddress() {
+    const [ip, setIp] = useState('127.0.0.1');
+    useEffect(() => {
+        fetch('https://jsonip.com').then(r => r.json()).then(data => {
+            console.log('Got ip address info', data);
+            setIp(data.ip);
+        })
+    }, []);
+    return <h4>Your Ip address is: <code>{ip}</code></h4>
+}
+
+export default function WhatIsMyIp() {
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>What Is My Ip Address</h2>
+                <MyIpAddress/>
+            </section>
+            <section>
+                <h3>
+                    <Link href={'/'}><a>Go back to home page</a></Link>
+                </h3>
+            </section>
+        </Layout>
+    )
+}
